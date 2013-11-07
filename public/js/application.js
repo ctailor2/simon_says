502fe9a0d89edb02f1543e608ef321276db1c894
dbc-apprentice <apprentice@devbootcamp.com>
2013-11-07 10:44:19 -0600

    completed

--- public/js/application.js
+++ public/js/application.js
@@ -1,3 +1,10 @@
 $(document).ready(function(){
-
+  $("#get_color").click(function(event){
+    $.post("/color", function(response){
+      var cell = response.cell.toString();
+      var color = response.color;
+      var selected_cell = $("li:nth-child(" + cell + ")");
+      selected_cell.css("backgroundColor", color);
+    });
+  });
 });
